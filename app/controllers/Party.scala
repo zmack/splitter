879a468851e8c20ebb3f196cea226392181082af
Andrei Bocan <zmaxor@gmail.com>
2013-10-28 01:02:33 +0200

    Adding mild persistence and a form thing.

--- app/controllers/Party.scala
+++ app/controllers/Party.scala
@@ -1,8 +1,15 @@
 package controllers
 
+import play.api.data.Form
+import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 import play.api.mvc._
 
 object Party extends Controller {
+  val partyForm: Form[models.Party] = Form(
+    mapping(
+      "title" -> nonEmptyText
+    )(models.Party.createFromForm)(models.Party.serializeToForm)
+  )
 
   def index = Action {
     val parties = models.Party.findAll(10)
@@ -14,4 +21,29 @@ object Party extends Controller {
     Ok(views.html.party.show(party))
   }
 
+  def update(id: Long) = Action {
+    val party = models.Party.findById(1)
+    Redirect(routes.Party.show(party.id))
+  }
+
+  def create = Action { implicit request =>
+    val form = partyForm.bindFromRequest()
+
+    form.fold(
+      hasErrors = { form =>
+        Redirect(routes.Party.new_party())
+      },
+      success = { newParty =>
+        models.Party.save(newParty)
+        Redirect(routes.Party.index())
+      }
+    )
+  }
+
+  def new_party = Action { implicit request =>
+    val form = partyForm.bindFromRequest()
+    Ok(views.html.party.new_party(form))
+  }
+
 }
+
--- app/models/Party.scala
+++ app/models/Party.scala
@@ -1,6 +1,7 @@
 package models
+import scala.collection.mutable.MutableList
 
-class Party(val id: Long, val user:User, val title: String) {
+case class Party(id: Long, user:User, title: String) {
   def members = {
     (0 to 10).map(n => User(n, s"Awesome guy $n from party $id", "some@email.com"))
   }
@@ -13,12 +14,29 @@ class Party(val id: Long, val user:User, val title: String) {
 
 object Party {
   val testUser = User(0, "Test guy", "test@email.com")
+  val partyPersistence = MutableList[Party]()
 
   def findAll(count:Int) = {
+    partyPersistence ++ createDynamic(count - partyPersistence.length)
+  }
+
+  def createDynamic(count:Int) = {
     (0 to count).map(n => new Party(n, testUser, s"This awesome party $n"))
   }
 
   def findById(id: Long) = {
     new Party(id, testUser, s"Awesome party $id")
+  }
+
+  def createFromForm(title: String) = {
+    new Party(0, null, title)
+  }
+
+  def serializeToForm(party:Party) = {
+    Some(party.title)
+  }
+
+  def save(party:Party) = {
+    partyPersistence += party
   }
 }
